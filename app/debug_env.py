ed82d217ff8b499278215b29fc6091d8f1738f60
Ubuntu <ubuntu@ip-172-31-17-248.ec2.internal>
2025-09-29 08:53:18 +0000

    app folder

--- app/debug_env.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print("API_URL:", os.getenv("API_URL"))
-print("RAPIDAPI_KEY:", os.getenv("RAPIDAPI_KEY"))
-print("RAPIDAPI_HOST:", os.getenv("RAPIDAPI_HOST"))
-
